b015828cb4bd6577ff5bda1367b5f74c70c2792c
Andrea Scarpino <andrea@archlinux.org>
2012-12-24 11:56:54 +0059

    Use anrxc's icons

--- .config/awesome/mine.lua
+++ .config/awesome/mine.lua
@@ -5,6 +5,7 @@ local awful = require("awful")
 
 theme = {}
 theme.confdir       = awful.util.getdir("config")
+local home          = os.getenv("HOME")
 
 theme.font          = "Menlo for Powerline 8"
 
@@ -35,13 +36,13 @@ theme.border_marked = "#91231c"
 --theme.taglist_bg_focus = "#ff0000"
 
 -- Display the taglist squares
-theme.taglist_squares_sel   = "/usr/share/awesome/themes/default/taglist/squarefw.png"
-theme.taglist_squares_unsel = "/usr/share/awesome/themes/default/taglist/squarew.png"
+theme.taglist_squares_sel   = theme.confdir .. "/icons/taglist/sel.png"
+theme.taglist_squares_unsel = theme.confdir .. "/icons/taglist/unsel.png"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = "/usr/share/awesome/themes/default/submenu.png"
+theme.menu_submenu_icon = theme.confdir .. "/icons/submenu.png"
 theme.menu_height = "15"
 theme.menu_width  = "100"
 
@@ -62,55 +63,55 @@ theme.widget_net    = theme.confdir .. "/icons/down.png"
 theme.widget_netup  = theme.confdir .. "/icons/up.png"
 theme.widget_wifi   = theme.confdir .. "/icons/wifi.png"
 theme.widget_mail   = theme.confdir .. "/icons/mail.png"
-theme.widget_mem   = theme.confdir .. "/icons/mem.png"
+theme.widget_mem    = theme.confdir .. "/icons/mem.png"
 theme.widget_music  = theme.confdir .. "/icons/music.png"
-theme.widget_cpu   = theme.confdir .. "/icons/cpu.png"
-theme.widget_vol   = theme.confdir .. "/icons/vol.png"
+theme.widget_cpu    = theme.confdir .. "/icons/cpu.png"
+theme.widget_vol    = theme.confdir .. "/icons/vol.png"
 theme.widget_date   = theme.confdir .. "/icons/time.png"
 -- }}}
 
 -- Define the image to load
-theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = theme.confdir .. "/icons/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = theme.confdir .. "/icons/titlebar/close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = theme.confdir .. "/icons/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = theme.confdir .. "/icons/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = theme.confdir .. "/icons/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = theme.confdir .. "/icons/titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = theme.confdir .. "/icons/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = theme.confdir .. "/icons/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = theme.confdir .. "/icons/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = theme.confdir .. "/icons/titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = theme.confdir .. "/icons/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = theme.confdir .. "/icons/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = theme.confdir .. "/icons/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = theme.confdir .. "/icons/titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = theme.confdir .. "/icons/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = theme.confdir .. "/icons/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = theme.confdir .. "/icons/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = theme.confdir .. "/icons/titlebar/maximized_focus_active.png"
 
 -- You can use your own command to set your wallpaper
-theme.wallpaper = "/home/andrea/Pictures/wallpapers/wall_120.jpg"
+theme.wallpaper = home .. "/Pictures/wallpapers/wall_120.jpg"
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
-theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
-theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floatingw.png"
-theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
-theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
-theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
-theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
-theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
-theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
-theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
+theme.layout_fairh = theme.confdir .. "/icons/layouts/fairh.png"
+theme.layout_fairv = theme.confdir .. "/icons/layouts/fairv.png"
+theme.layout_floating  = theme.confdir .. "/icons/layouts/floating.png"
+theme.layout_magnifier = theme.confdir .. "/icons/layouts/magnifier.png"
+theme.layout_max = theme.confdir .. "/icons/layouts/max.png"
+theme.layout_fullscreen = theme.confdir .. "/icons/layouts/fullscreen.png"
+theme.layout_tilebottom = theme.confdir .. "/icons/layouts/tilebottom.png"
+theme.layout_tileleft   = theme.confdir .. "/icons/layouts/tileleft.png"
+theme.layout_tile = theme.confdir .. "/icons/layouts/tile.png"
+theme.layout_tiletop = theme.confdir .. "/icons/layouts/tiletop.png"
+theme.layout_spiral  = theme.confdir .. "/icons/layouts/spiral.png"
+theme.layout_dwindle = theme.confdir .. "/icons/layouts/dwindle.png"
 
-theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
+theme.awesome_icon = theme.confdir .. "/icons/awesome.png"
 
 -- Define the icon theme for application icons. If not set then the icons 
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
--- .vim/ftplugin/java.vim
+++ .vim/ftplugin/java.vim
@@ -4,7 +4,7 @@ map <C-S-F> :%JavaFormat<CR>
 map <C-S-R> :JavaRename 
 map <C-F11> :JavaSearchva %<CR>
 map <S-A-J> :JavaDocComment<CR>
-map <S-A-O> :JavaRenameavaImportOrganize<CR>
+map <S-A-O> :JavaImportOrganize<CR>
 map <S-F2> :JavaDocSearch<CR>
 map <F2> :JavaDocPreview<CR>
 map <F3> :JavaSearchContext<CR>
