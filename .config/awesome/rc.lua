bbcdec5d2797e5e5f5e0215fd31a1b5b09b7b932
Andrea Scarpino <me@andreascarpino.it>
2013-09-09 09:34:52 +0200

    [awesome] Show focused task

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -324,6 +324,7 @@ for s = 1, screen.count() do
 
     -- Create a tasklist widget
     -- mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)
+    mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.focused)
 
     -- Create the wibox
     mywibox[s] = awful.wibox({ position = "top", screen = s })
@@ -365,7 +366,7 @@ for s = 1, screen.count() do
     -- Now bring it all together (with the tasklist in the middle)
     local layout = wibox.layout.align.horizontal()
     layout:set_left(left_layout)
-    --layout:set_middle(mytasklist[s])
+    layout:set_middle(mytasklist[s])
     layout:set_right(right_layout)
 
     mywibox[s]:set_widget(layout)
